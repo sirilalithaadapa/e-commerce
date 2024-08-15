c88d72013890d5cac8199668b2299d5b76233f48
sirilalithaadapa <sirilalitha9088@gmail.com>
2024-08-15 19:04:37 +0530

    bulk updates

--- script.js
+++ script.js
@@ -6,39 +6,147 @@ function showPopup(product) {
         product1: {
             img: 'saree1.jpg',
             title: 'Saree 1',
-            price: '$50.00'
+            price: '$50.00',
+            type: 'silk'
         },
         product2: {
             img: 'saree2.jpg',
             title: 'Saree 2',
-            price: '$60.00'
+            price: '$60.00',
+            type: 'cotton'
         },
         product3: {
             img: 'saree3.jpg',
             title: 'Saree 3',
-            price: '$55.00'
+            price: '$55.00',
+            type: 'georgette'
         },
         product4: {
             img: 'saree4.jpg',
             title: 'Saree 4',
-            price: '$75.00'
+            price: '$75.00',
+            type: 'silk'
         },
         product5: {
             img: 'saree5.jpg',
             title: 'Saree 5',
-            price: '$58.00'
+            price: '$58.00',
+            type: 'cotton'
         },
         product6: {
             img: 'saree6.jpg',
             title: 'Saree 6',
-            price: '$88.00'
+            price: '$88.00',
+            type: 'georgette'
         },
-        product7: {
-            img: 'saree7.jpg',
-            title: 'Saree 7',
-            price: '$70.00'
+        banarasi1: {
+            img: 'Images/Banarasi/saree1.jpg',
+            title: 'Banarasi Saree 1',
+            price: '$100.00',
+            type: 'banarasi'
+        },
+        banarasi2: {
+            img: 'Images/Banarasi/saree2.jpg',
+            title: 'Banarasi Saree 2',
+            price: '$120.00',
+            type: 'banarasi'
+        },
+        banarasi3: {
+            img: 'Images/Banarasi/saree3.jpg',
+            title: 'Banarasi Saree 3',
+            price: '$150.00',
+            type: 'banarasi'
+        },
+        banarasi4: {
+            img: 'Images/Banarasi/saree4.jpg',
+            title: 'Banarasi Saree 4',
+            price: '$140.00',
+            type: 'banarasi'
+        },
+        banarasi5: {
+            img: 'Images/Banarasi/saree5.jpg',
+            title: 'Banarasi Saree 5',
+            price: '$160.00',
+            type: 'banarasi'
+        },
+        kanchivaram1: {
+            img: 'Images/Kanjivaram/saree1.jpg',
+            title: 'Kanchivaram Saree 1',
+            price: '$150.00',
+            type: 'kanchivaram'
+        },
+        kanchivaram2: {
+            img: 'Images/Kanjivaram/saree2.jpg',
+            title: 'Kanchivaram Saree 2',
+            price: '$180.00',
+            type: 'kanchivaram'
+        },
+        kanchivaram3: {
+            img: 'Images/Kanjivaram/saree3.jpg',
+            title: 'Kanchivaram Saree 3',
+            price: '$1800.00',
+            type: 'kanchivaram'
+        },
+        kanchivaram4: {
+            img: 'Images/Kanjivaram/saree4.jpg',
+            title: 'Kanchivaram Saree 4',
+            price: '$1500.00',
+            type: 'kanchivaram'
+        },
+        kanchivaram5: {
+            img: 'Images/Kanjivaram/saree5.jpg',
+            title: 'Kanchivaram Saree 5',
+            price: '$1200.00',
+            type: 'kanchivaram'
+        },
+        kanchivaram6: {
+            img: 'Images/Kanjivaram/saree6.jpg',
+            title: 'Kanchivaram Saree 6',
+            price: '$1100.00',
+            type: 'kanchivaram'
+        },
+        kanchivaram7: {
+            img: 'Images/Kanjivaram/saree7.jpg',
+            title: 'Kanchivaram Saree 7',
+            price: '$1800.00',
+            type: 'kanchivaram'
+        },
+        chanderi1: {
+            img: 'Images/chanderi/saree1.jpg',
+            title: 'Chanderi Saree 1',
+            price: '$1300.00',
+            type: 'chanderi'
+        },
+        chanderi2: {
+            img: 'Images/chanderi/saree2.jpg',
+            title: 'Chanderi Saree 2',
+            price: '$1500.00',
+            type: 'chanderi'
+        },
+        chanderi3: {
+            img: 'Images/chanderi/saree3.jpg',
+            title: 'Chanderi Saree 3',
+            price: '$1400.00',
+            type: 'chanderi'
+        },
+        chanderi4: {
+            img: 'Images/chanderi/saree4.jpg',
+            title: 'Chanderi Saree 4',
+            price: '$1300.00',
+            type: 'chanderi'
+        },
+        chanderi5: {
+            img: 'Images/chanderi/saree5.jpg',
+            title: 'Chanderi Saree 5',
+            price: '$1400.00',
+            type: 'chanderi'
+        },
+        chanderi6: {
+            img: 'Images/chanderi/saree6.jpg',
+            title: 'Chanderi Saree 6',
+            price: '$1800.00',
+            type: 'chanderi'
         }
-
     };
 
     currentProduct = products[product];
@@ -100,4 +208,26 @@ function updateQuantity(index, action) {
 
     document.getElementById('cart-count').textContent = cartItems.reduce((total, item) => total + item.quantity, 0);
     openCartPopup();
+}
+document.getElementById('priceRange').addEventListener('input', function () {
+    let minPrice = this.min;
+    let maxPrice = this.max;
+    let value = this.value;
+    document.getElementById('min-price').textContent = minPrice;
+    document.getElementById('max-price').textContent = value;
+
+    filterProductsByPrice(minPrice, value);
+});
+
+function filterProductsByPrice(min, max) {
+    const products = document.querySelectorAll('.product');
+    products.forEach(product => {
+        const priceText = product.querySelector('p').textContent.replace('$', '');
+        const price = parseFloat(priceText);
+        if (price >= min && price <= max) {
+            product.style.display = 'block';
+        } else {
+            product.style.display = 'none';
+        }
+    });
 }
